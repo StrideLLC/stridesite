6c54454a3407507a0570d458f784346c05050ecd
Zach <113333499+ZachGTheG@users.noreply.github.com>
2023-07-18 00:48:39 -0400

    pouusgbskdhgksd

--- src/app/components/buttons/navtoggle.js
+++ src/app/components/buttons/navtoggle.js
@@ -18,11 +18,31 @@ export const MenuToggle = ({ toggle }) => {
     setIsOpen(!isOpen);
   };
 
+  const nav = [
+    {
+      name: "About",
+      link: "#about",
+    },
+    {
+      name: "tools",
+      link: "#projects",
+    },
+  ];
+
   return (
     <LayoutGroup>
       <AnimatePresence>
-        <motion.div className="relative " initial={{opacity: 0, y: 40}} whileInView={{opacity: 1, y: 0}} viewport={{once: true}} transition={{type: 'spring', stiffness: 300, damping: 10}}>
-          <motion.button className="z-20 absolute top-1/2 left-0 transform -translate-y-1/2 scale-[1.5]" onClick={toggleMenu}>
+        <motion.div
+          className="relative "
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", stiffness: 300, damping: 10 }}
+        >
+          <motion.button
+            className="z-20 absolute top-1/2 left-0 transform -translate-y-1/2 scale-[1.5]"
+            onClick={toggleMenu}
+          >
             <motion.svg
               layout
               width="23"
@@ -55,13 +75,71 @@ export const MenuToggle = ({ toggle }) => {
             </motion.svg>
           </motion.button>
           <div className="absolute z-10 ">
-            <motion.div className="bg-purple-400/50 flex py-12 w-40 h-48 -ml-32 -mt-4"  initial={{opacity: 0}} animate={isOpen ? "open" : "closed"} variants={{closed: {opacity: 0, y: -100}, open: {opacity: 1, y: 0}}}>
-              <ul className="">
-                <li>
-                  <a href="#about" className="text-white hover:text-purple-500">wip</a>
-                </li>
-              </ul>
-            </motion.div>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  className="md:bg-purple-400/50 bg-purple-500 flex py-12 md:w-52 md:h-48 w-screen -ml-[20.5rem] h-56  md:-ml-44 md:-mt-6  -mt-8 rounded-xl"
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={{
+                    initial: { opacity: 0, y: -100, filter: "blur(10px)" },
+                    animate: {
+                      opacity: 1,
+                      y: 0,
+                      filter: "blur(0px)",
+                      transition: {
+                        when: "beforeChildren",
+                        staggerChildren: 0.1,
+                      },
+                    },
+                    exit: {
+                      opacity: 0,
+                      y: -100,
+                      filter: "blur(10px)",
+                      transition: {
+                        when: "afterChildren",
+                        staggerChildren: 0.1,
+                        staggerDirection: -1,
+                      },
+                    },
+                  }}
+                >
+                  <ul className="text-2xl ml-4 font-bold">
+                    {nav.map((item, index) => (
+                      <motion.li
+                        key={index}
+                        variants={{
+                          initial: {
+                            opacity: 0,
+                            filter: "blur(10px)",
+                            transform: "scale(0.5)",
+                          },
+                          animate: {
+                            opacity: 1,
+                            filter: "blur(0px)",
+                            transform: "scale(1)",
+                            transition: { delay: 0.1 * index },
+                          },
+                          exit: {
+                            filter: "blur(10px)",
+                            opacity: 0,
+                            transform: "scale(0.5)",
+                          },
+                        }}
+                      >
+                        <a
+                          href={item.link}
+                          className="text-white hover:text-purple-500"
+                        >
+                          {item.name}
+                        </a>
+                      </motion.li>
+                    ))}
+                  </ul>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         </motion.div>
       </AnimatePresence>
